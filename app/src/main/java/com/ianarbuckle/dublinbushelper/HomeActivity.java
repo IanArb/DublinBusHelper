43c047cb55b378a4d7ec6a8a383a3a0c752a9fc5
Ian Arbuckle <ian.arbuckle18@gmail.com>
2017-02-19 21:08:14 +0000

    Develop branch setup

---
+++ app/src/main/java/com/ianarbuckle/dublinbushelper/HomeActivity.java
@@ -0,0 +1,9 @@
+package com.ianarbuckle.dublinbushelper;
+
+/**
+ * Created by Ian Arbuckle on 14/02/2017.
+ *
+ */
+
+public class HomeActivity {
+}
---
+++ app/src/main/java/com/ianarbuckle/dublinbushelper/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.ianarbuckle.dublinbushelper;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ian Arbuckle on 14/02/2017.
+ *
+ */
+
+public class HomeFragment extends BaseFragment {
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    return super.onCreateView(inflater, container, savedInstanceState);
+  }
+
+  @Override
+  protected void initPresenter() {
+
+  }
+}
