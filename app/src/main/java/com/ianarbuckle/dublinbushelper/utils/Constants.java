4b0ec3d139858616a55ba4cb1b75ccb94f4ffa5f
Ian Arbuckle <ian.arbuckle18@gmail.com>
2017-03-02 10:29:28 +0000

    DB-Map (#2)
    * Setup location services
    
    * - Implemented network calls to RTPI API for Dublin Bus stops
    - Location Helper class for decoupling of google map logic
    
    * - Deleted old folder
    
    * Setup location services
    
    * - Deleted old folder
    
    * - Deleted old folder
    
    * - Deleted old folder

--- app/src/main/java/com/ianarbuckle/dublinbushelper/utils/Constants.java
+++ app/src/main/java/com/ianarbuckle/dublinbushelper/utils/Constants.java
@@ -17,4 +17,5 @@ public class Constants {
   public static final String ROUTES_KEY = "routes";
 
   public static final String DEBUG_LOGGER = "TAG";
+  public static final String ERROR_DIALOG_FRAGMENT = "errorDialog";
 }
