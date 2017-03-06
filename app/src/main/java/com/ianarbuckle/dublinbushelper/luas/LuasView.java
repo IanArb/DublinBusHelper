b950a33ae79ccc7482a690678e144a38e69e1a69
Ian Arbuckle <ian.arbuckle18@gmail.com>
2017-03-06 11:01:59 +0000

    TH-luas (#3)
    * - Login/Registration with Firebase Auth
    - Luas Adapter with RecyclerView
    
    * - Custom activity slide animations

---
+++ app/src/main/java/com/ianarbuckle/dublinbushelper/luas/LuasView.java
@@ -0,0 +1,12 @@
+package com.ianarbuckle.dublinbushelper.luas;
+
+import android.content.Context;
+
+/**
+ * Created by Ian Arbuckle on 02/03/2017.
+ *
+ */
+
+public interface LuasView {
+  Context getContext();
+}
