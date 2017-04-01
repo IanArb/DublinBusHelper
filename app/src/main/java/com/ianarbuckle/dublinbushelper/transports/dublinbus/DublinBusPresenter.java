3b12d9adf9ed49f1dd0a35a05cb97bc50d39df8b
Ian Arbuckle <ian.arbuckle18@gmail.com>
2017-04-01 21:22:41 +0100

    TH-Luas (#4)
    * - Login/Registration with Firebase Auth
    - Luas Adapter with RecyclerView
    
    * - Custom activity slide animations
    
    * - Implemented real time logic for stations, luas stops and bus stops
    
    * - Implemented real time logic for stations, luas stops and bus stops

--- app/src/main/java/com/ianarbuckle/dublinbushelper/transports/dublinbus/DublinBusPresenter.java
+++ app/src/main/java/com/ianarbuckle/dublinbushelper/transports/dublinbus/DublinBusPresenter.java
@@ -3,6 +3,8 @@ package com.ianarbuckle.dublinbushelper.transports.dublinbus;
 import android.support.v4.app.Fragment;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.ianarbuckle.dublinbushelper.models.MarkerItemModel;
+import com.ianarbuckle.dublinbushelper.models.stopinfo.Result;
 
 /**
  * Created by Ian Arbuckle on 19/02/2017.
@@ -10,11 +12,9 @@ import com.google.android.gms.maps.GoogleMap;
  */
 
 public interface DublinBusPresenter {
-  void setView(DublinBusView view);
-  void setFragmentView(DublinBusFragmentView view);
   boolean checkLocationPermission(Fragment fragment);
   void onRequestPermission();
   void initMap(GoogleMap googleMap);
   void fetchStops();
-  void sendToDatabase(String lastUpdate, String stopid, String name, String routes, float lat, float lon);
+  MarkerItemModel getMarkerItems(Result result);
 }
